bd14f5aa7e2069376126c58345d0ad56e76c160e
Seung Joo <psjoo3515@gmail.com>
2024-10-04 16:07:29 +0900

    Setting scanBasePackages

--- src/main/java/io/springdb2/Springdb2Application.java
+++ src/main/java/io/springdb2/Springdb2Application.java
@@ -1,13 +1,24 @@
 package io.springdb2;
 
+import io.springdb2.config.MemoryConfig;
+import io.springdb2.domain.item.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "io.springdb2.web")
 public class Springdb2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Springdb2Application.class, args);
+    }
+
+    @Bean
+    @Profile("local")
+    public TestDataInit testDataInit(ItemRepository itemRepository) {
+        return new TestDataInit(itemRepository);
     }
 
 }
