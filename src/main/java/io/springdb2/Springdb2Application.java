a47b7bb9d1b5a57e38d068c8cacf5ccd451de04a
Seung Joo <psjoo3515@gmail.com>
2024-10-07 15:23:04 +0900

    Using SimpleJdbcInsert

--- src/main/java/io/springdb2/Springdb2Application.java
+++ src/main/java/io/springdb2/Springdb2Application.java
@@ -1,8 +1,6 @@
 package io.springdb2;
 
-import io.springdb2.config.JdbcTemplateV1Config;
-import io.springdb2.config.JdbcTemplateV2Config;
-import io.springdb2.config.MemoryConfig;
+import io.springdb2.config.JdbcTemplateV3Config;
 import io.springdb2.domain.item.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +10,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
-@Import(JdbcTemplateV2Config.class)
+//@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "io.springdb2.web")
 public class Springdb2Application {
     public static void main(String[] args) {
