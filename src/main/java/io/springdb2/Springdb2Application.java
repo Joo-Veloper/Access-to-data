dd48c9b3b4866f5584715bc9dc1e4e1e4f5c95c4
Seung Joo <psjoo3515@gmail.com>
2024-10-07 15:15:12 +0900

    Using JdbcTemplateV2Config

--- src/main/java/io/springdb2/Springdb2Application.java
+++ src/main/java/io/springdb2/Springdb2Application.java
@@ -1,6 +1,7 @@
 package io.springdb2;
 
 import io.springdb2.config.JdbcTemplateV1Config;
+import io.springdb2.config.JdbcTemplateV2Config;
 import io.springdb2.config.MemoryConfig;
 import io.springdb2.domain.item.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class)
-@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "io.springdb2.web")
 public class Springdb2Application {
     public static void main(String[] args) {
---
+++ src/main/java/io/springdb2/config/JdbcTemplateV2Config.java
@@ -0,0 +1,30 @@
+package io.springdb2.config;
+
+
+import io.springdb2.domain.item.repository.ItemRepository;
+import io.springdb2.domain.item.service.ItemService;
+import io.springdb2.domain.item.service.ItemServiceV1;
+import io.springdb2.jdbctemplate.JdbcTemplateItemRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV2Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV2(dataSource);
+    }
+
+}
