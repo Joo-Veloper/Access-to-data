68f11cfd5295edc5e01986766a8d591e71fac0ee
Seung Joo <psjoo3515@gmail.com>
2024-10-07 14:03:46 +0900

    Run JDBC Configuration

--- src/main/java/io/springdb2/Springdb2Application.java
+++ src/main/java/io/springdb2/Springdb2Application.java
@@ -1,5 +1,6 @@
 package io.springdb2;
 
+import io.springdb2.config.JdbcTemplateV1Config;
 import io.springdb2.config.MemoryConfig;
 import io.springdb2.domain.item.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Import(MemoryConfig.class)
+//@Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
 @SpringBootApplication(scanBasePackages = "io.springdb2.web")
 public class Springdb2Application {
     public static void main(String[] args) {
