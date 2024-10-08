ba3e35584746e88d6893d2936084a08d3a9f9a54
Seung Joo <psjoo3515@gmail.com>
2024-10-08 14:29:40 +0900

    Embedded Mode DB

--- src/main/java/io/springdb2/Springdb2Application.java
+++ src/main/java/io/springdb2/Springdb2Application.java
@@ -2,16 +2,21 @@ package io.springdb2;
 
 import io.springdb2.config.JdbcTemplateV3Config;
 import io.springdb2.domain.item.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
 @Import(JdbcTemplateV3Config.class)
+@Slf4j
 @SpringBootApplication(scanBasePackages = "io.springdb2.web")
 public class Springdb2Application {
     public static void main(String[] args) {
@@ -24,4 +29,15 @@ public class Springdb2Application {
         return new TestDataInit(itemRepository);
     }
 
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        log.info("메모리 데이터베이스 초기화");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
 }
