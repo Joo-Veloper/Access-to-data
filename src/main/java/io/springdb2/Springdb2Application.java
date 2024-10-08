6f2355611e67abbc5c7b3999432ac38439c15b40
Seung Joo <psjoo3515@gmail.com>
2024-10-08 14:51:34 +0900

    Embedded Mode(SpringBoot) DB

--- src/main/java/io/springdb2/Springdb2Application.java
+++ src/main/java/io/springdb2/Springdb2Application.java
@@ -29,7 +29,7 @@ public class Springdb2Application {
         return new TestDataInit(itemRepository);
     }
 
-    @Bean
+   /* @Bean
     @Profile("test")
     public DataSource dataSource() {
         log.info("메모리 데이터베이스 초기화");
@@ -39,5 +39,5 @@ public class Springdb2Application {
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
-    }
+    }*/
 }
