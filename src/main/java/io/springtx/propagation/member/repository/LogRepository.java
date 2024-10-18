d29c2287a36fe91860027254c57f958319ace33e
Seung Joo <psjoo3515@gmail.com>
2024-10-18 15:53:26 +0900

    Transaction propagation commit and rollback

--- src/main/java/io/springtx/propagation/member/repository/LogRepository.java
+++ src/main/java/io/springtx/propagation/member/repository/LogRepository.java
@@ -20,8 +20,7 @@ public class LogRepository {
     public void save(Log logMessage) {
         log.info("log 저장");
         em.persist(logMessage);
-
-        if (logMessage.getMessage().contains("로그인 예외")) {
+        if (logMessage.getMessage().contains("로그예외")) {
             log.info("log 저장시 예외 발생");
             throw new RuntimeException("예외 발생");
         }
