c913da9631b7d40f73b13902bc4eea7b3b58157b
Seung Joo <psjoo3515@gmail.com>
2024-10-20 17:37:07 +0900

    MemberService apply Transaction

--- src/main/java/io/springtx/propagation/member/service/MemberService.java
+++ src/main/java/io/springtx/propagation/member/service/MemberService.java
@@ -7,6 +7,7 @@ import io.springtx.propagation.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -16,29 +17,30 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
 
+    @Transactional
     public void joinV1(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
 
-        log.info("==memberRepository 호출 시작==");
+        log.info("== memberRepository 호출 시작 ==");
         memberRepository.save(member);
-        log.info("==memberRepository 호출 종료==");
+        log.info("== memberRepository 호출 종료 ==");
 
-        log.info("==logRepository 호출 시작==");
+        log.info("== logRepository 호출 시작 ==");
         logRepository.save(logMessage);
-        log.info("==logRepository 호출 종료==");
+        log.info("== logRepository 호출 종료 ==");
     }
 
+    @Transactional
     public void joinV2(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
 
-        log.info("==memberRepository 호출 시작==");
+        log.info("== memberRepository 호출 시작 ==");
         memberRepository.save(member);
-        log.info("==memberRepository 호출 종료==");
+        log.info("== memberRepository 호출 종료 ==");
 
-        log.info("==logRepository 호출 시작==");
-
+        log.info("== logRepository 호출 시작 ==");
         try {
             logRepository.save(logMessage);
         } catch (RuntimeException e) {
@@ -46,6 +48,6 @@ public class MemberService {
             log.info("정상 흐름 반환");
         }
 
-        log.info("==logRepository 호출 종료==");
+        log.info("== logRepository 호출 종료 ==");
     }
 }
